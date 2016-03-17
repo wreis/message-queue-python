63905b95148074ff0f33eb3491a7cdb33cd16031
Vitor Leal <vitorleal1@gmail.com>
2016-03-17 10:47:06 -0300

    add not acknowledge to worker

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='message-queue',
-    version='0.1.1',
+    version='0.1.6',
     description='Message Queue',
     long_description='Message Queue python library to publish and subscribe to queues with diferent types of adapters.',
     url='https://github.com/ingresse/message-queue-python',
@@ -10,7 +10,7 @@ setuptools.setup(
     author_email='vitor.leal@ingresse.com',
     license='BSD',
     packages=setuptools.find_packages(),
-    install_requires=['pika'],
-    download_url = 'https://github.com/ingresse/message-queue-python/tarball/0.1.1',
+    install_requires=['pika>=0.10.0'],
+    download_url = 'https://github.com/ingresse/message-queue-python/tarball/0.1.6',
 )
 
