ab2c03be0cdddd7a0c94e81e7e702bb49ccb550e
Vitor Leal <vitorleal1@gmail.com>
2016-03-17 17:06:17 -0300

    fix setup py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='message-queue',
-    version='0.1.5',
+    version='0.1.6',
     description='Message Queue',
     long_description='Message Queue python library to publish and subscribe to queues with diferent types of adapters.',
     url='https://github.com/ingresse/message-queue-python',
@@ -10,8 +10,7 @@ setuptools.setup(
     author_email='vitor.leal@ingresse.com',
     license='BSD',
     packages=setuptools.find_packages(),
-    install_requires=open('requirements.txt').read(),
-    download_url = 'https://github.com/ingresse/message-queue-python/tarball/0.1.5',
-    zip_safe=True,
+    install_requires=['pika>=0.10.0'],
+    download_url = 'https://github.com/ingresse/message-queue-python/tarball/0.1.6',
 )
 
